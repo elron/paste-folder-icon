e0c0cec6c440a13649b6ae726a5802ac9811fddb
Elron <elron100duck@gmail.com>
2021-08-26 05:18:44 +0300

    COUT WORKS!

--- test.cpp
+++ test.cpp
@@ -1,12 +1,34 @@
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <windows.h>
 #include <iostream>
-using namespace std;
+using  namespace std; // …
 
-int main() {
-  cout << "Hello World!";
-  return 0;
-}
-int main() {
-  cout << "Hello World!";
-  return 0;
+// DWORD GetFileAttributesA(
+//   LPCSTR lpFileName='C:\\Users'
+// );
+
+// int main() {
+//   cout << "asd";
+// }
+int main()
+{
+  cout << GetFileAttributesA("C:\\Users\\elron\\Elron Apps C\\005 Folder Icon\\paste-folder-icon\\test\\test");
 }
 
+// int main()
+// {
+//   GetFileAttributesA('C:\\Users\\elron\\Elron Apps C\\005 Folder Icon\\paste-folder-icon\\test\\test');
+//   return 3;
+// }
+
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
+// int main() {
+//   cout << "Hello World!";
+//   return 0;
+// }
